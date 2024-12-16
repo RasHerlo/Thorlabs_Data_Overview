11ed86ca14926ff4c2e3e894d57ea3b6a5dc474e
Rasmus Herlo <rasmusherlo@gmail.com>
2024-12-16 22:21:26 +0100

    trials

---
+++ Trial_Module.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 16 21:52:47 2024
+
+@author: rasmu
+"""
+
+def trial_func(a,b):
+    c = a + b
+    return c
+    
+    
---
+++ Trial_Run.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Dec 16 22:00:44 2024
+
+@author: rasmu
+"""
+
+
+from Trial_Module import trial_func
+
+c = trial_func(3, 4)
+
+print(c)
