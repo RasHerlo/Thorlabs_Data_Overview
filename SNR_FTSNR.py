2baa53ab86e065163f4f9e50f08de30c261ce69d
Rasmus Herlo <rasmusherlo@gmail.com>
2024-12-17 21:04:08 +0100

    New Stuff

---
+++ SNR_FTSNR.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Dec 17 20:03:35 2024
+
+@author: rasmu
+"""
+
+with open('my_file.txt', 'w') as file:
+    file.write('Hello, world!\n')
+    file.write('This is a new line.\n')
